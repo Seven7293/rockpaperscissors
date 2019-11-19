121e6dca2b2fde82ccac5c45ae757da3e9b84029
Seven7293 <Seven.dickens@gmail.com>
2019-11-19 23:18:59 +0000

    Finally done w/ the randomizer

--- js/script.js
+++ js/script.js
@@ -8,4 +8,18 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+$(document).ready(function() {
+    $("#shoot").click(function(){
+        $("#userChoice").text($("#input").val());
+    var com = Math.random();
 
+    console.log(com);
+    if (com < 1 && com > .66666) {
+        $("#computerChoice").text("Rock");
+    } else if (com < .66666 && com > .33333){
+        $("#computerChoice").text("Paper");
+    } else {
+        $("#computerChoice").text("Scissors")};
+    })
+    //DOM manipulation code
+});
